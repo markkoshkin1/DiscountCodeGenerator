d1b99240a8f5f797f5f20f9858bee5035bc6f1c4
Mark Koshkin <mdcmark3@gmail.com>
2025-06-30 21:06:33 +0200

    Update cors and settings

--- backend/DiscountCodeGenerator/DiscountCodeGenerator.API/Program.cs
+++ backend/DiscountCodeGenerator/DiscountCodeGenerator.API/Program.cs
@@ -21,6 +21,15 @@ namespace DiscountCodeGenerator.API
 
             // Add services to the container.   
             builder.Services.AddGrpc();
+            builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+            {
+                builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+            }));
+
+
             builder.Services.AddDbContextPool<DiscountCodeContext>(options =>
                              options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -39,7 +48,7 @@ namespace DiscountCodeGenerator.API
             // Configure the HTTP request pipeline.
             app.UseGrpcWeb();
             app.MapGrpcService<GreeterService>();
-            app.MapGrpcService<DiscountService>().EnableGrpcWeb();
+            app.MapGrpcService<DiscountService>().EnableGrpcWeb().RequireCors("AllowAll"); ;
             app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
             try
