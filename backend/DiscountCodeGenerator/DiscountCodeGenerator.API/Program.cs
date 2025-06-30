a2bbe67a40a8a2270a25aeaac9cdbf9f166b8f38
Mark Koshkin <mdcmark3@gmail.com>
2025-06-30 21:00:33 +0200

    Add grpc web

--- backend/DiscountCodeGenerator/DiscountCodeGenerator.API/Program.cs
+++ backend/DiscountCodeGenerator/DiscountCodeGenerator.API/Program.cs
@@ -37,7 +37,9 @@ namespace DiscountCodeGenerator.API
             }
 
             // Configure the HTTP request pipeline.
+            app.UseGrpcWeb();
             app.MapGrpcService<GreeterService>();
+            app.MapGrpcService<DiscountService>().EnableGrpcWeb();
             app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
             try
